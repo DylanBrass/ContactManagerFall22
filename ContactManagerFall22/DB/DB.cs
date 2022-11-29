d1f0394a86f3ca34dcbe044e1271021e87384634
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2022-11-29 13:00:48 -0500

    Added starting classed for database

---
+++ ContactManagerFall22/DB/DB.cs
@@ -0,0 +1,32 @@
+﻿using ContactManagerFall22.DB.Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ContactManagerFall22.DB
+{
+    internal class DB
+    {
+        //GetContacts
+        //GetContact
+        //GetAddresses
+        //GetAddress
+
+        SqlConnection s;
+
+        public DB()
+        {
+            s = new SqlConnection();
+        }
+        public List<Contact> GetContacts()
+        {
+            return null;
+        }
+
+        public Contact GetContact(int id)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ ContactManagerFall22/DB/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactManagerFall22.DB.Entities
+{
+    internal class Contact
+    {
+        public Contact(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
