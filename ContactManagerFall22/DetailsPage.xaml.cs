83e52348b5a28a419699e2968118f5f05f9ac94e
HennCH <2132132@champlaincollege.qc.ca>
2022-12-06 14:29:39 -0500

    Tryind to make the detailPage work part 2

--- ContactManagerFall22/DetailsPage.xaml.cs
+++ ContactManagerFall22/DetailsPage.xaml.cs
@@ -11,17 +11,4 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
-namespace ContactManagerFall22
-{
-    /// <summary>
-    /// Interaction logic for DetailsPage.xaml
-    /// </summary>
-    public partial class DetailsPage : Window
-    {
-        public DetailsPage()
-        {
-            InitializeComponent();
-        }
-    }
-}
+nd
