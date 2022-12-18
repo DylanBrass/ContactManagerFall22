2381c2423a32d2a2b3e43a450f3c7cc1446c0f7c
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2022-12-18 14:28:40 -0500

    add address page

--- ContactManagerFall22/AddAdressPage.xaml.cs
+++ ContactManagerFall22/AddAdressPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContactManagerFall22.DB;
+using ContactManagerFall22.DB.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace ContactManagerFall22
 {
@@ -19,14 +22,47 @@ namespace ContactManagerFall22
     /// </summary>
     public partial class AddAdressPage : Window
     {
+
+       readonly DBManager dB = new DBManager();
         public AddAdressPage()
         {
             InitializeComponent();
         }
 
-        private void AddContactButton_Click(object sender, RoutedEventArgs e)
+        private void AddAddressButton_Click(object sender, RoutedEventArgs e)
         {
+            if (String.IsNullOrEmpty(AddressNumber.Text.ToString()) || String.IsNullOrEmpty(Street.Text) || (String.IsNullOrEmpty(AppartementNum.Text.ToString()) || String.IsNullOrEmpty(City.Text)))
+            {
+                this.Close();
+            }
+            else
+            {
+                Address addingAddress = new Address();
 
+                var addressNumber = int.TryParse(AddressNumber.Text, out int number);
+                addingAddress.AddressNumber = number;
+
+                addingAddress.Street = Street.Text;
+
+                var appNum = int.TryParse(AppartementNum.Text, out int appNumber);
+                addingAddress.ApartementNum = appNumber;
+
+                addingAddress.City = City.Text;
+                addingAddress.Country = Country.Text;
+                addingAddress.AreaCode = AreaCode.Text;
+
+
+                dB.CreateAddress(addingAddress);
+                this.Close();
+            }
+        }
+
+        private void Window_closed(object sender, EventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
         }
     }
 }
+
+
