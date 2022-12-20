fbd3a91c744e567d50bb478f279ad97562011878
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2022-12-20 14:25:21 -0500

    error push

--- ContactManagerFall22/PhoneDetails.xaml.cs
+++ ContactManagerFall22/PhoneDetails.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ContactManagerFall22.DB;
 using ContactManagerFall22.DB.Entities;
 using System.Collections.Generic;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -24,16 +25,20 @@ namespace ContactManagerFall22
         {
             Phone phone = dbManager3.GetPhone(Id);
 
-
+            PhoneNumber.Content = "Phone Number: " + phone.PhoneNumber.ToString();
+            Type.Content = "Type: " + phone.Type;
+            Description.Content = "Description: " + phone.Desc;
+            DateCreated.Content = "DateAdded: " + phone.DateCreated.ToShortDateString();
+            LastUpdated.Content = "DateUpdated: " + phone .LastUpdated.ToShortDateString();
 
         }
 
-        private void Delete_Address(object sender, RoutedEventArgs e)
+        private void Delete_Phone(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this Address?", "Confirmation", MessageBoxButton.YesNo);
             if (result == MessageBoxResult.Yes)
             {
-                dbManager3.DeleteAddress(Id);
+                dbManager3.DeletePhone(Id);
                 this.Close();
             }
 
